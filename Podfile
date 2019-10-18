b1f2a89a08b68e966fb2757155d2a7d7fa64f6f1
yangyu <yangyu2010@aliyun.com>
2019-10-18 16:07:42 +0800

    创建项目

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '11.0'
+
+target 'WebViewDownloadFile' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WebViewDownloadFile
+  pod 'Alamofire'
+
+end
---
+++ WebViewDownloadFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewDownloadFile
+//
+//  Created by YangYu on 2019/10/18.
+//  Copyright © 2019 YangYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
